37f2c2e314c70dac9430237dccc2cd4c20c927dd
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-02-19 22:54:48 +0530

    String

---
+++ day5_String/String.js
@@ -0,0 +1,75 @@
+//String is group of character.
+//255 character
+
+let s1 = "sanjay"
+let s2 = 'Sharma';
+let s3 = `webDev is most beautiful lesson`;
+let age = 22;
+let degree = "Btech"
+let loggedinPersonname = "ajay"
+let text = 'most'
+let replaceText='handeled'
+
+
+// console.log(s3.replace(text,replaceText));
+// console.log(s3);
+// console.log(s3.toUpperCase())
+// console.log(s3.toLowerCase())
+// console.log(s3.toLocaleLowerCase())
+// console.log(s3.toLocaleUpperCase());
+// console.log(s3.search("unique!!"))
+//It return -1 because of  unique !! not
+//present  
+
+// console.log(s3.search('most'));
+//It represents starting index number.
+
+// let s4 = 'dmsandja djsadhjsakdjkdjsadbjdsafkjsa nkjskjfs
+//  njfdsfj nfkdskds'
+
+
+
+// let newStr = `${s1} ${s2} is learn javaScript.I  am  studying in ${degree}.My current age is ${age}`;
+// console.log(newStr);
+// let logIn = `Hello, ${loggedinPersonname}`;
+// console.log(logIn);
+
+// console.log(s1+" "+s2);
+
+// for(let i=0;i<s3.length;i++){
+//     console.log(s3[i])
+// }
+
+// console.log(s2.charAt(4));
+// console.log(s1[3]);
+// console.log(typeof s1,typeof s2,typeof s3);
+
+
+
+//slice, substring,substr
+
+//slice
+
+// let str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+// let str2 = 'Hello World!!!'
+
+// let str3 = "john"
+
+// let str = "What I'd like to tell on this topic is."
+// let newStr = str.slice(0,20);
+// console.log(newStr+'...');
+
+// let str2 = "Hi everyone!"
+// let newStr2 = str2.substring(-3);
+// console.log(newStr2);
+
+
+
+// let newStr = str.slice(0,149);
+// let newStr = str.substring(0,149);
+// let newStr = str.substring(149);
+// let newStr = str2.slice(-10,-4);
+// let newStr = str2.substring(-5);
+//In substring if we put - value then it consider as 0.
+// console.log(newStr + 'Read More...')
